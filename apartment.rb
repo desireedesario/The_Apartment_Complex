9218c3e013a53abbef4d2625f30ad30b40e017a4
Desiree Desario <desireedesario@gmail.com>
2016-07-12 16:43:38 -0700

    added student boolean

--- apartment.rb
+++ apartment.rb
@@ -1,8 +1,20 @@
 class Apartment
-  def initialize unit, num_beds, num_baths, tenants
-    @unit = unit
-    @num_beds = num_beds
-    @num_baths = num_baths
-    @tenants = tenants
-  end
+		attr_accessor :rate
+	  def initialize unit, num_beds, num_baths, tenants
+	    @unit = unit
+	    @num_beds = num_beds
+	    def price
+	    	rate = @num_beds * 1000 + @num_baths * 500
+	    	print "$#{rate}"
+	    end
+	    def studio?
+	    	if @num_beds == 1
+	    		true
+	    	else
+	    		false
+	    end
+	    @num_baths = num_baths
+	    @tenants = tenants
+	  end
+	end
 end
